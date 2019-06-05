c17bc9b5f97d160432d400b0b41517e3d68cc7f8
mnorty <mnorty@gmail.com>
2019-06-05 14:12:10 -0600

    Basic Task display

--- src/Components/ManagerSpecificProject/ManagerSpecificProject.js
+++ src/Components/ManagerSpecificProject/ManagerSpecificProject.js
@@ -1,12 +1,19 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
-
+import './ManagerSpecificProject.css'
 
 const ManagerSpecificProject = () => {
     return <>
-    <h1>
-        ManagerSpecificProject
-    </h1>
+        <div>
+            <header className='pageHeader'>Project Name</header>
+            <div className='assigned'>Assigned Tasks
+                <div className='taskDisplay'></div>
+                <Link to='/ManagerCreateTask'>Create Project</Link>
+            </div>
+            <div className='unassigned'>Unassigned Tasks
+                <div className='taskDisplay'></div>
+            </div>
+        </div>
     </>
 }
 
