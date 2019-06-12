69e77682e09f07d2a2a7cf631beb928adb7c732b
mnorty <mnorty@gmail.com>
2019-06-12 13:17:33 -0600

    starting styling of Registration

--- src/Components/Register/Register.js
+++ src/Components/Register/Register.js
@@ -5,8 +5,11 @@ import './Register.css'
 const Register = () => {
     return (
         <div className='registrationContainer'>
-            <header className='pageHeader'>Registration</header>
-            <div className='infoBox'> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis labore dolorum velit quis eaque illum totam recusandae provident, distinctio, sed quae nihil consequatur cum quam officiis accusantium? Quis, nam laudantium?</div>
+            <div className='top'>
+                <header>
+                    <h1 className='welcome'>Create Account</h1>
+                </header>
+            </div>
             <RegisterForm/>
         </div>
         )
