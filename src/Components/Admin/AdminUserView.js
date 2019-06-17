7ab1a50e53a57db8d8eabb13edfc840ef0e92f10
mnorty <mnorty@gmail.com>
2019-06-17 10:36:33 -0600

    small update

--- src/Components/Admin/AdminUserView.js
+++ src/Components/Admin/AdminUserView.js
@@ -31,7 +31,6 @@ class AdminUserView extends Component {
       <div className='news-container'>
         {this.props.loading ? <Loading /> : <div>{users}</div>}
       </div>
-      <h1></h1>
       </>
     )
   }
